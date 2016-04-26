dd51cbf363a402880aeee3f3eef21eee6584d6bb
Tree Casiano <tree.pdx@gmail.com>
2016-04-26 07:08:31 -0700

    Add filter function to select different buslines and change the color on mouseover.

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
-var map = L.map('map').setView([45.509330, -122.677826], 11);
-var VancouverMarker = L.marker([45.638728, -122.661486]).addTo(map);
-var OCHINmarker = L.marker([45.509330, -122.677826]).addTo(map);
+var map = L.map('map').setView([45.509330, -122.670476], 11);
+var VancouverMarker = L.marker([45.638728, -122.670476]).addTo(map);
+var OCHINmarker = L.marker([45.5094603, -122.677897]).addTo(map);
 
 VancouverMarker.bindPopup("<b>Downtown Vancouver</b>").openPopup();
 OCHINmarker.bindPopup("<b>OCHIN</b>").openPopup();
@@ -16,30 +16,55 @@ L.tileLayer(
     }).addTo(map);
 
 // Adding layers with D3
-
 d3.json("buslines.geojson", function(collection) {
-
-    var buslines = [collection];
+    console.log(collection.features);
+    var buslines = collection.features.filter(function(item){
+        return item.properties["ROUTE"] == 6 || item.properties["ROUTE"] == 56;
+    });
     L.geoJson(buslines, {
-        onEachFeature: onEachFeature
+        onEachFeature: onEachFeature,
+        style: style
     }).addTo(map);
 
-    overlays["geojson_d3"] = geojson_d3;
-    L.control.layers(baseLayers, overlays).addTo(map);
+    L.control.layers().addTo(map);
 });
 
 function onEachFeature(feature, layer) {
     if (feature.properties) {
-        layer.bindPopup('<b>Bus Line: ' + Number(feature.properties.RTE) + '</b>');
+        var popup = L.popup();
+        // TODO: show popup on mouseover
+        popup.setContent('<b>Bus Line: ' + feature.properties.ROUTE + '</b>');
+        layer.on('mouseover', function() {
+            layer.setStyle({color : 'yellow', opacity: '1'});
+            layer.bindPopup(popup);
+        });
+        layer.on('mouseout', function() {
+            layer.setStyle(style(feature));
+        })
     }
 }
 
-// var popup = L.popup();
-// function onMapClick(e) {
-//     popup
-//         .setLatLng(e.latlng)
-//         .setContent("You clicked the map at " + e.latlng.toString())
-//         .openOn(map);
-// }
+function style(feature) {
+    // TODO: edit conditional statement so that buses are each a different color
+    if (feature.properties.ROUTE == "56") {
+        return {
+            color: 'mediumturquoise',
+            weight: 2,
+            opacity: .5
+        }
+    }
+    else {
+        return {
+            color: 'blue',
+            weight: 2,
+            opacity: .5
+        }
+    }
 
-// map.on('click', onMapClick);
+}
+
+// TODO: change color of individual buslines
+// TODO: add bus stop shapefiles (clip with QGIS?)
+// TODO: select lines/stops that are within walking distance of OCHIN
+// TODO: style markers and popups
+// TODO: create legend
