2a2e96398a41e322fb2a68bcf4130bfbea542735
Tree Casiano <tree.pdx@gmail.com>
2016-04-30 18:14:08 -0700

    Add light rail and legend.

--- map.js
+++ map.js
@@ -1,14 +1,34 @@
 var map = L.map('map').setView([45.509330, -122.670476], 11);
 var VancouverMarker = L.marker([45.638728, -122.670476]).addTo(map);
 var OCHINmarker = L.marker([45.5094603, -122.677897]).addTo(map);
-
-VancouverMarker.bindPopup("<b>Downtown Vancouver</b>").openPopup();
-OCHINmarker.bindPopup("<b>OCHIN</b>").openPopup();
-
+var legend = L.control({
+    position: 'topright'
+});
 var mapLink =
     '<a href="http://openstreetmap.org">OpenStreetMap</a>';
 var wholink =
     '<a href="http://stamen.com">Stamen Design</a>';
+
+VancouverMarker.bindPopup("<b>Downtown Vancouver</b>").openPopup();
+OCHINmarker.bindPopup("<b>OCHIN</b>").openPopup();
+
+legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend'),
+        labels = ['<strong>Transit Types</strong>'],
+        categories = ['Bus','Light Rail'];
+    for (var i = 0; i < categories.length; i++) {
+
+        div.innerHTML +=
+            labels.push(
+                '<i class="legend-icon" style="background-color:' + getColor(categories[i]) + '"></i> ' +
+                (categories[i] ? categories[i] : '+'));
+
+    }
+    div.innerHTML = labels.join('<br>');
+    return div;
+};
+legend.addTo(map);
+
 L.tileLayer(
     'http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
         attribution: '&copy; ' + mapLink + ' Contributors & ' + wholink,
@@ -17,54 +37,75 @@ L.tileLayer(
 
 // Adding layers with D3
 d3.json("buslines.geojson", function(collection) {
-    console.log(collection.features);
     var buslines = collection.features.filter(function(item){
-        return item.properties["ROUTE"] == 6 || item.properties["ROUTE"] == 56;
+        return item.properties["ROUTE"] == 6;
     });
     L.geoJson(buslines, {
-        onEachFeature: onEachFeature,
-        style: style
+        onEachFeature: onEachBuslineFeature,
+        style: styleBus
     }).addTo(map);
-
-    L.control.layers().addTo(map);
 });
 
-function onEachFeature(feature, layer) {
+d3.json("lrt_line.geojson", function(collection) {
+    var maxLines = collection.features.filter(function(item){
+        return item.properties["TYPE"];
+    });
+    L.geoJson(maxLines, {
+        onEachFeature: onEachMaxFeature,
+        style: styleMax
+    }).addTo(map);
+});
+
+function onEachBuslineFeature(feature, layer) {
     if (feature.properties) {
         var popup = L.popup();
         // TODO: show popup on mouseover
         popup.setContent('<b>Bus Line: ' + feature.properties.ROUTE + '</b>');
+        layer.bindPopup(popup);
         layer.on('mouseover', function() {
             layer.setStyle({color : 'yellow', opacity: '1'});
-            layer.bindPopup(popup);
         });
         layer.on('mouseout', function() {
-            layer.setStyle(style(feature));
+            layer.setStyle(styleBus(feature));
         })
     }
 }
 
-function style(feature) {
-    // TODO: edit conditional statement so that buses are each a different color
-    if (feature.properties.ROUTE == "56") {
-        return {
-            color: 'mediumturquoise',
-            weight: 2,
-            opacity: .5
-        }
+function onEachMaxFeature(feature, layer) {
+    if (feature.properties) {
+        var popup = L.popup();
+        // TODO: show popup on mouseover
+        popup.setContent('<b>Max Line: ' + feature.properties["LINE"] + '</b>');
+        layer.bindPopup(popup);
+        layer.on('mouseover', function() {
+            layer.setStyle({color : 'orange', opacity: '1'});
+        });
+        layer.on('mouseout', function() {
+            layer.setStyle(styleMax(feature));
+        })
     }
-    else {
-        return {
-            color: 'blue',
-            weight: 2,
-            opacity: .5
-        }
-    }
-
 }
 
-// TODO: change color of individual buslines
-// TODO: add bus stop shapefiles (clip with QGIS?)
-// TODO: select lines/stops that are within walking distance of OCHIN
+function styleBus(feature) {
+    return {
+        color: 'mediumturquoise',
+        weight: 3,
+        opacity: .75
+    }
+}
+
+function styleMax(feature) {
+    return {
+        color: 'fuchsia',
+        weight: 3,
+        opacity: .75
+    }
+}
+
+function getColor(d) {
+    return d === 'Bus' ? "mediumturquoise" : d === 'Light Rail' ? "fuchsia" : "#ff7f00";
+}
+
+// TODO: add more buslines and change color of individual buslines
+// TODO: add bus stop shapefiles within walking distance from OCHIN (clip with QGIS?)
 // TODO: style markers and popups
-// TODO: create legend
